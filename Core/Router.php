f2baf6f12f375cf28d5f7888b28fc08faad6f5ba
Oussama Bel <oussamabelfarhi@gmail.com>
2023-11-09 18:37:01 +0100

    AmCorrection du Router.php

--- Core/Router.php
+++ Core/Router.php
@@ -1,6 +1,6 @@
 <?php
 
-declare (strict_types = 1);
+declare (strict_types=1);
 
 namespace Core;
 
@@ -8,8 +8,8 @@ namespace Core;
  * Router class responsible for parsing the URL, determining the route,
  * and dispatching the request to the corresponding controller action.
  */
-
-class Router {
+class Router
+{
 
     /**
      * Namespace for admin controllers.
@@ -21,7 +21,8 @@ class Router {
     /**
      * Parses the current URL and initiates the dispatch process.
      */
-    public static function run(): void {
+    public static function run(): void
+    {
         list($controllerName, $action, $params) = self::parseUrl();
 
         $controllerClass = self::getControllerClass($controllerName);
@@ -39,7 +40,8 @@ class Router {
      *
      * @return array An array consisting of the controller name, action, and parameters.
      */
-    private static function parseUrl(): array {
+    private static function parseUrl(): array
+    {
         $url = $_SERVER['QUERY_STRING'] ?: 'home/index';
         $params = explode('/', $url);
 
@@ -56,7 +58,8 @@ class Router {
      *
      * @param array $params URL segments that may contain the admin namespace.
      */
-    private static function setAdminNamespace(array &$params): void {
+    private static function setAdminNamespace(array &$params): void
+    {
         if ($params[0] === 'admin') {
             self::$adminNamespace = array_shift($params);
         }
@@ -68,7 +71,8 @@ class Router {
      * @param string $controllerName The name of the controller.
      * @return string The fully qualified class name of the controller.
      */
-    private static function getControllerClass(string $controllerName): string {
+    private static function getControllerClass(string $controllerName): string
+    {
         $namespace = self::$adminNamespace ? "App\\Controllers\\Admin\\" : "App\\Controllers\\";
         return $namespace . $controllerName . 'Controller';
     }
@@ -81,7 +85,8 @@ class Router {
      * @param array $params Parameters for the controller method.
      * @return bool True if the route is valid, false otherwise.
      */
-    private static function isValidRoute(string $controllerClass, string $action, array $params): bool {
+    private static function isValidRoute(string $controllerClass, string $action, array $params): bool
+    {
         return class_exists($controllerClass) && method_exists($controllerClass, $action) && self::paramsMatchRequirements($controllerClass, $action, $params);
     }
 
@@ -93,7 +98,8 @@ class Router {
      * @param array $params Parameters for the controller method.
      * @return bool True if the parameters match the method's requirements, false otherwise.
      */
-    private static function paramsMatchRequirements(string $controllerClass, string $action, array $params): bool {
+    private static function paramsMatchRequirements(string $controllerClass, string $action, array $params): bool
+    {
         $method = new \ReflectionMethod($controllerClass, $action);
         $numberOfRequiredParams = $method->getNumberOfRequiredParameters();
         return count($params) === $numberOfRequiredParams;
@@ -106,7 +112,8 @@ class Router {
      * @param string $action The controller method to be called.
      * @param array $params Parameters for the controller method.
      */
-    private static function executeControllerAction(string $controllerClass, string $action, array $params): void {
+    private static function executeControllerAction(string $controllerClass, string $action, array $params): void
+    {
         $controller = new $controllerClass();
         $params = self::castParamsToMethodTypes(new \ReflectionMethod($controllerClass, $action), $params);
         $controller->$action(...$params);
@@ -119,12 +126,13 @@ class Router {
      * @param array $params Parameters to be casted.
      * @return array The casted parameters.
      */
-    private static function castParamsToMethodTypes(\ReflectionMethod $method, array $params): array {
+    private static function castParamsToMethodTypes(\ReflectionMethod $method, array $params): array
+    {
         foreach ($method->getParameters() as $index => $param) {
             if ($param->hasType() && isset($params[$index])) {
                 $paramType = $param->getType()->getName();
                 if ($paramType === 'int') {
-                    $params[$index] = (int) $params[$index];
+                    $params[$index] = (int)$params[$index];
                 }
             }
         }
